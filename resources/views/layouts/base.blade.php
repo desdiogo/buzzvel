a2c6cd7ca527967b310e1a8db528cbbc6994ad13
Diogo Eleutero <desdiogo10@yahoo.com>
2023-03-15 09:41:50 -0300

    fix theme

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +11,7 @@
         @vite('resources/css/app.css')
     </head>
     <body>
-        <div class="relative min-h-screen bg-gray-900 w-full p-8 flex justify-center">
+        <div class="relative min-h-screen w-full p-8 flex justify-center">
             @yield('content')
         </div>
         @livewireScripts
